9a44dd10292e2287a04ca3a8bbe04db25c78c80f
icefirebear <woungsub1234@gmail.com>
2022-03-02 18:00:40 +0900

    Change funciton to syncronize

--- export_html.py
+++ export_html.py
@@ -1,7 +1,8 @@
 import mpld3
+from functions import *
 
 
-async def export_html(plot, title):
+def export_html(plot, title):
     source = mpld3.fig_to_html(plot, figid="{}_id".format(title))
-    with await open("{}.txt".format(title), "w", encoding="utf-8") as f:
+    with open("./{}.txt".format(title), "w", encoding="utf-8") as f:
         f.writelines(source)
