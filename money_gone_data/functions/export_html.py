536d080d67cb33ee7ee6edd4a245cc0470d1ac97
icefirebear <woungsub1234@gmail.com>
2022-03-03 14:13:07 +0900

    change file structure

--- money_gone_data/functions/export_html.py
+++ money_gone_data/functions/export_html.py
@@ -4,5 +4,5 @@ import mpld3
 def export_html(plot, title):
     title = title[10:]
     source = mpld3.fig_to_html(plot, figid="{}_id".format(title))
-    with open("./{}.txt".format(title), "w", encoding="utf-8") as f:
+    with open("/app/money_gone/data/{}.txt".format(title), "w", encoding="utf-8") as f:
         f.writelines(source)
