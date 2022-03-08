0859beaa8f085981be984ce9604bd7ba61b34134
icefirebear <woungsub1234@gmail.com>
2022-03-08 18:02:47 +0900

    edit

--- money_gone_data/functions/export_html.py
+++ money_gone_data/functions/export_html.py
@@ -4,5 +4,18 @@ import mpld3
 def export_html(plot, title):
     title = title[10:]
     source = mpld3.fig_to_html(plot, figid="{}_id".format(title))
-    with open("/app/money_gone/data/{}.txt".format(title), "w", encoding="utf-8") as f:
+    with open(
+        "/app/money_gone/data/{}.html".format(title), "w+", encoding="utf-8"
+    ) as f:
         f.writelines(source)
+    with open("/app/money_gone/data/index.html", "a", encoding="utf-8") as f:
+        start, end = 0
+        templates = f.readlines()
+        for i, data in enumerate(templates):
+            if status and ("_id" in data):
+                end = i
+                continue
+            if title + "_id" in data:
+                start = i
+                status = True
+        templates[start:end] = source
