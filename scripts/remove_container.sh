4e5b559a89b0a86d715407a20d415c07155054d2
변웅섭 <50494659+icefirebear@users.noreply.github.com>
2022-03-11 17:59:29 +0900

    Update remove_container.sh

--- scripts/remove_container.sh
+++ scripts/remove_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker container stop $(docker container ls -q --filter name=money_gone*)
-docker rm bus3013/money_gone:latest
+docker rm $(docker container ls -q --filter name=money_gone*)
