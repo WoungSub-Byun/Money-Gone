432851763c738e28a8bff6ef7b6640c9501e862a
icefirebear <woungsub1234@gmail.com>
2022-03-13 23:23:02 +0900

    edit source

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import time
 from flask import Flask, render_template
 import load_data
+
 
 app = Flask(__name__)
 
 
-@app.route("/index", methods=["GET"])
+@app.route("/", methods=["GET"])
 def show_tables():
+    start_at = time.time()
     load_data.load_data()
-    path = "/app/money_gone/data"
-    return render_template("/app/money_gone/data/index.html")
+    end_at = time.time()
+    print(
+        "start_at: {}\n end_at: {}\nloading time..: {}".format(
+            start_at, end_at, end_at - start_at
+        )
+    )
+    data_dir = "/app/money_gone/data/"
+    return render_template("{}index.html".format(data_dir))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="localhost", port=4000, debug=True)
