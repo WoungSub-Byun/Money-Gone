1539d207fcb3bf0902899892a721561e0cb36666
icefirebear <woungsub1234@gmail.com>
2022-03-16 02:07:49 +0900

    handle hotfix

--- data/functions/export_html.py
+++ data/functions/export_html.py
@@ -6,18 +6,25 @@ def export_html(plot, title):
     source = mpld3.fig_to_html(plot, figid="{}_id".format(title))
     with open("/data/{}.html".format(title), "w+", encoding="utf-8") as f:
         f.writelines(source)
-    with open("/data/index.html", "r", encoding="utf-8") as fr:
-        start = 0
-        end = 0
-        status = False
-        templates = fr.readlines()
-        for i, data in enumerate(templates):
-            if status and ("_id" in data):
-                end = i
-                continue
-            if title + "_id" in data:
-                start = i
-                status = True
+    try:
+        with open("/data/index.html", "r", encoding="utf-8") as fr:
+            start = 0
+            end = 0
+            status = False
+            templates = fr.readlines()
+            for i, data in enumerate(templates):
+                if status and ("_id" in data):
+                    end = i
+                    continue
+                if title + "_id" in data:
+                    start = i
+                    status = True
+    except Exception as e:
+        print(e)
+        initial = True
     with open("/data/index.html", "w+", encoding="utf-8") as fw:
-        templates[start:end] = source
-        fw.writelines(templates)
+        if initial:
+            fw.writelines(source)
+        else:
+            templates[start:end] = source
+            fw.writelines(templates)
