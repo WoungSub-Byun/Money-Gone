dacab8c53b2810f90465ce7f878c76c3942d410e
icefirebear <woungsub1234@gmail.com>
2022-03-15 23:41:25 +0900

    move location

--- data/functions/export_html.py
+++ data/functions/export_html.py
@@ -4,9 +4,9 @@ import mpld3
 def export_html(plot, title):
     title = title[10:]
     source = mpld3.fig_to_html(plot, figid="{}_id".format(title))
-    with open("/money_gone/data/{}.html".format(title), "w+", encoding="utf-8") as f:
+    with open("/data/{}.html".format(title), "w+", encoding="utf-8") as f:
         f.writelines(source)
-    with open("/money_gone/data/index.html", "w+", encoding="utf-8") as f:
+    with open("/data/index.html", "w+", encoding="utf-8") as f:
         start, end = 0
         templates = f.readlines()
         for i, data in enumerate(templates):
