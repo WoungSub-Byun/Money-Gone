f06126f436327ad154d8639cd4873c00a2503796
icefirebear <woungsub1234@gmail.com>
2022-03-16 00:39:28 +0900

    add cmds on scripts

--- scripts/reset_env.sh
+++ scripts/reset_env.sh
@@ -5,4 +5,7 @@ docker stop money_gone_was
 docker rm money_gone_was
 docker rm money_gone_data
 
+docker rmi bus3013/money_gone_data:latest
+docker rmi bus3013/money_gone_was:latest
+
 rm -rf /home/money_gone_data
