6cfe21ed843dcfb12d49059443db1238ede1a64d
icefirebear <woungsub1234@gmail.com>
2022-03-16 02:13:35 +0900

    handle hotfix

--- data/functions/export_html.py
+++ data/functions/export_html.py
@@ -4,6 +4,7 @@ import mpld3
 def export_html(plot, title):
     title = title[10:]
     source = mpld3.fig_to_html(plot, figid="{}_id".format(title))
+    is_initial = True
     with open("/data/{}.html".format(title), "w+", encoding="utf-8") as f:
         f.writelines(source)
     try:
@@ -21,10 +22,11 @@ def export_html(plot, title):
                     status = True
     except Exception as e:
         print(e)
-        initial = True
+        is_initial = True
     with open("/data/index.html", "w+", encoding="utf-8") as fw:
-        if initial:
+        if is_initial:
             fw.writelines(source)
+            is_initial = False
         else:
             templates[start:end] = source
             fw.writelines(templates)
